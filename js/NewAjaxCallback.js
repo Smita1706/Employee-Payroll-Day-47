eb1b1b95763490d7785cb118c53b42ed2bc2d322
Smita1706 <smitachinchkar1998@gmail.com>
2022-04-05 18:18:42 +0530

    Refactored

--- js/NewAjaxCallback.js
+++ js/NewAjaxCallback.js
@@ -1,11 +1,12 @@
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; 
+//let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; 
    function makePromiseCall (methodType, url, async = true, data = null) {
      return new Promise(function (resolve, reject) {
        let xhr = new XMLHttpRequest();
-       xhr.onreadystatechange = function(){ 
-         console.log("State Changed Called. Ready State: "+ 
+       xhr.onload = function(){ 
+         console.log(methodType+"State Changed Called. Ready State: "+ 
                     xhr.readyState+" Status:"+xhr.status);
-           if (xhr.status.toString().match('^[2] [0-9]{2}$')) { resolve(xhr.responseText);
+           if (xhr.status.toString().match('^[2] [0-9]{2}$')) { 
+               resolve(xhr.responseText);
            } else if (xhr.status.toString().match('^[4, 5] [0-9]{2}$')) { 
                reject( {
                   status: xhr.status,
@@ -14,9 +15,17 @@ let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
                console.log("XHR Failed");
             }
         }
+        xhr.onerror = function(){
+            reject({
+                status : this.status,
+                statusText:XMLHttpRequest.statusText
+            });
+        };
         xhr.open(methodType, url, async);
         if (data) {
-        console.log(JSON.stringify(data)); xhr.setRequestHeader("Content-Type", "application/json"); xhr.send(JSON.stringify(data));
+          console.log(JSON.stringify(data)); 
+          xhr.setRequestHeader("Content-Type", "application/json");
+           xhr.send(JSON.stringify(data));
         } else xhr.send();
         console.log(methodType+" request sent to the server");
     });
